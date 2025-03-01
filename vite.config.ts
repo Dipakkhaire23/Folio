9533e2c225ae1138f0a49751be301be06e9f8e0d
Dipakkhaire23 <dipakkhaire23@gmail.com>
2025-03-01 13:15:15 +0530

    namechange

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // Replace with your actual repository name
+  base: '/Folio/', // Replace with your actual repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
